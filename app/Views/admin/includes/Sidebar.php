9c532b9fe6e0803e3fbfbe9c54f0f5c6088db830
waguri <thai.lebachkhoa@hcmut.edu.vn>
2026-05-14 21:51:48 +0700

    fku merge commit

--- app/Views/admin/includes/Sidebar.php
+++ app/Views/admin/includes/Sidebar.php
@@ -21,36 +21,37 @@ if (!function_exists('admin_render_sidebar')) {
     function admin_render_sidebar()
     {
 ?>
-        <div class="sidebar-menu">
-            <div class="sidebar-header">
-                <div class="logo">
-                    <a href="<?= BASE_URL ?>/admin">
-                        <i class="fa-solid fa-leaf admin-brand-icon"></i>
-                        <span>Plantify Admin</span>
-                    </a>
-                </div>
-            </div>
-            <div class="main-menu">
-                <div class="menu-inner">
-                    <nav>
-                        <ul class="metismenu" id="menu">
-                            <?php admin_sidebar_item('', 'ti-dashboard', 'Dashboard', 'admin'); ?>
-                            <?php admin_sidebar_item('shop_settings', 'ti-shopping-cart-full', 'Cấu hình Cửa hàng', 'shop-settings'); ?>
-                            <?php admin_sidebar_item('pages', 'ti-layout-media-center', 'Nội dung Trang Giới Thiệu', 'pages'); ?>
-                            <?php admin_sidebar_item('news', 'ti-agenda', 'Quản lý Tin tức', 'news'); ?>
-                            <?php admin_sidebar_item('comments', 'ti-comments-smiley', 'Bình luận', 'comments'); ?>
-                            <?php admin_sidebar_item('faqs', 'ti-help-alt', 'FAQ', 'faqs'); ?>
-                            <?php admin_sidebar_item('users', 'ti-user', 'Thành viên', 'users'); ?>
-                            <?php admin_sidebar_item('rag', 'ti-comments', 'Dữ liệu bot', 'rag'); ?>
-                            <?php admin_sidebar_item('products', 'ti-package', 'Quản lý Sản phẩm', 'products'); ?>
-                            <li>
-                                <a href="<?= BASE_URL ?>"><i class="ti-home"></i><span>Xem website</span></a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
+<div class="sidebar-menu">
+    <div class="sidebar-header">
+        <div class="logo">
+            <a href="<?= BASE_URL ?>/admin">
+                <i class="fa-solid fa-leaf admin-brand-icon"></i>
+                <span>Plantify Admin</span>
+            </a>
         </div>
+    </div>
+    <div class="main-menu">
+        <div class="menu-inner">
+            <nav>
+                <ul class="metismenu" id="menu">
+                    <?php admin_sidebar_item('', 'ti-dashboard', 'Dashboard', 'admin'); ?>
+                    <?php admin_sidebar_item('shop_settings', 'ti-shopping-cart-full', 'Cấu hình Cửa hàng', 'shop-settings'); ?>
+                    <?php admin_sidebar_item('contacts', 'ti-email', 'Liên hệ từ khách hàng', 'contacts'); ?>
+                    <?php admin_sidebar_item('pages', 'ti-layout-media-center', 'Nội dung Trang Giới Thiệu', 'pages'); ?>
+                    <?php admin_sidebar_item('news', 'ti-agenda', 'Quản lý Tin tức', 'news'); ?>
+                    <?php admin_sidebar_item('comments', 'ti-comments-smiley', 'Bình luận', 'comments'); ?>
+                    <?php admin_sidebar_item('faqs', 'ti-help-alt', 'FAQ', 'faqs'); ?>
+                    <?php admin_sidebar_item('users', 'ti-user', 'Thành viên', 'users'); ?>
+                    <?php admin_sidebar_item('rag', 'ti-comments', 'Dữ liệu bot', 'rag'); ?>
+                    <?php admin_sidebar_item('products', 'ti-package', 'Quản lý Sản phẩm', 'products'); ?>
+                    <li>
+                        <a href="<?= BASE_URL ?>"><i class="ti-home"></i><span>Xem website</span></a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
 <?php
     }
 }
